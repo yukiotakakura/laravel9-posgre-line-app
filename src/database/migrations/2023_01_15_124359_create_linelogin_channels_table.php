1fb4bf83f8189c6c4dbd324f740fee703a677fae
shibata0318 <yt031821@gmail.com>
2023-01-22 17:35:42 +0900

    PHP CS Fixer解消

--- src/database/migrations/2023_01_15_124359_create_linelogin_channels_table.php
+++ src/database/migrations/2023_01_15_124359_create_linelogin_channels_table.php
@@ -4,18 +4,15 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class() extends Migration {
     /**
      * Run the migrations.
-     *
-     * @return void
      */
     public function up(): void
     {
         Schema::create('linelogin_channels', function (Blueprint $table) {
             $table->id()->comment('ID');
-            $table->foreignId('linedeveloper_provider_id')->constrained('linedevelopers_providers');;
+            $table->foreignId('linedeveloper_provider_id')->constrained('linedevelopers_providers');
             // BIGINTEGER型、符号無し、ユニーク制約
             $table->bigInteger('channel_id')->unsigned()->unique()->comment('チャンネルID');
             $table->string('name', 20)->comment('チャンネル名');
@@ -26,8 +23,6 @@ return new class extends Migration
 
     /**
      * Reverse the migrations.
-     *
-     * @return void
      */
     public function down(): void
     {
