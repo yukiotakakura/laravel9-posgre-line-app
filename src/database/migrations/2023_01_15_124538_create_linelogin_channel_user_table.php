fcd32149fdf87bfd9708cb44f17c6ecd947276df
shibata0318 <yt031821@gmail.com>
2023-01-23 00:04:03 +0900

    削除

--- src/database/migrations/2023_01_15_124538_create_linelogin_channel_user_table.php
+++ src/database/migrations/2023_01_15_124538_create_linelogin_channel_user_table.php
@@ -10,7 +10,7 @@ return new class() extends Migration {
      */
     public function up(): void
     {
-        Schema::create('user_linelogin_channel', function (Blueprint $table) {
+        Schema::create('linelogin_channel_user', function (Blueprint $table) {
             $table->id()->comment('ID');
             $table->foreignId('user_id')->constrained('users');
             $table->foreignId('linelogin_channel_id')->constrained('linelogin_channels');
